8d4b5e30b10eb2b6194b86d4ac76c6046ad6ec1c
Naoki Yoshimi <nyoshimi@c3r3s8.42tokyo.jp>
2024-09-14 20:32:32 +0900

    アルゴリズム実装完了

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: nyoshimi <nyoshimi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/13 14:24:14 by yoshiminaok       #+#    #+#             */
-/*   Updated: 2024/09/13 23:41:44 by nyoshimi         ###   ########.fr       */
+/*   Updated: 2024/09/14 20:23:23 by nyoshimi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,10 +35,15 @@ int main(int argc,char **argv)
     }
     pthread_create(&monitor_thread,NULL,monitor_routine,&tool);
     tool.index = 0;
-    while(tool.index < tool.fork_num)
+    if(tool.fork_num == 1)
+        pthread_detach(philos[tool.index].thread);
+    else
     {
-        pthread_join(philos[tool.index].thread,NULL);
-        tool.index++; 
+        while(tool.index < tool.fork_num)
+        {
+            pthread_join(philos[tool.index].thread,NULL);
+            tool.index++; 
+        }
     }
     pthread_join(monitor_thread,NULL);
     return (0);
@@ -72,12 +77,11 @@ void *run_philosopher_loop(void *arg)
     t_philo *philo;
     
     philo = (t_philo *)arg;
-    if (philo->id % 2 == 1)
-        usleep(200);
+    if (philo->id % 2 == 0)
+        usleep(300);
     while(philo->tool->philos_alive)
     {
         do_sycle(philo->tool,philo);
-        // break;
     }
     return (NULL);
 }
@@ -89,6 +93,21 @@ long long get_current_time()
     return (tv.tv_sec * 1000LL) + (tv.tv_usec / 1000LL);
 }
 
+int track_meals(t_tool *tool,t_philo *philos)
+{
+    int i;
+
+    i = 0;
+    if(!tool->eat_time)
+        return(0);
+    while(i < tool->fork_num)
+    {
+        if (philos[i].eat_time < tool->eat_time)
+            return (0);
+        i++;
+    }
+    return (1);
+}
 void *monitor_routine(void *arg)
 {
     t_tool *tool;
@@ -106,9 +125,14 @@ void *monitor_routine(void *arg)
                  tool->philos_alive = 0;
                  break;
             }
+            if(track_meals(tool,tool->philos))
+            {
+                printf("%lld All the philosophers have eaten at least %ld times.\n",get_current_time(),tool->eat_time);
+                tool->philos_alive = 0;
+                break;
+            }
         }
         usleep(10000);
-        
     }
     return (NULL);
 }
